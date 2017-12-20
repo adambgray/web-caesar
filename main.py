d724e6d4a3f0c4506be1ab999ea66d51c86f9d45
Adam Gray <adambgray@gmail.com>
2017-12-19 20:20:17 -0500

    modified main to format rotated string in textarea, added .vscode to .gitignore

--- main.py
+++ main.py
@@ -10,25 +10,25 @@ form = """
 <html>
     <head>
         <style>
-            form {
+            form {{
                 background-color: #eee;
                 padding: 20px;
                 margin: 0 auto;
                 width: 540px;
                 font: 16px sans-serif;
                 border-radius: 10px;
-            }
-            textarea {
+            }}
+            textarea {{
                 margin: 10px 0;
                 width: 540px;
                 height: 120px;
-            }
+            }}
         </style>
     </head>
     <body>
         <form method='post'>
             <input type='text' label='Rotate by:' name='rot' value='0'>
-            <textarea name='text'></textarea>
+            <textarea name='text'>{0}</textarea>
             <input type='submit' />
         </form>
     </body>
@@ -38,14 +38,13 @@ form = """
 
 @app.route("/")
 def index():
-    return form
+    return form.format('')
 
 @app.route("/", methods=['POST'])
 def encrypt():
     rot = request.form['rot']
     rot = int(rot)
     text = request.form['text']
-    return '<h1>' + rotate_string(text, rot) + '</h1>'
-
+    return form.format(rotate_string(text, rot))
 
 app.run()
